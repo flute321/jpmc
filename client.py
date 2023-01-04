47d02cf1d857f18f2e2b7a70592ee3c70e277eef
flute321 <suchitastargirl@gmail.com>
2023-01-04 14:49:13 -0500

    task 1

--- client.py
+++ client.py
@@ -35,14 +35,16 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
-    return stock, bid_price, ask_price, price
+    price = bid_price+ask_price
+    return stock, bid_price, ask_price, price/2
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    if(price_b == 0):
+        return 0
+    return price_a/price_b
 
 
 # Main
